7c4b06739a1ed3453908e04132ed7496ad8f4c03
delta_kimigatame <sankaku.kuro@gmail.com>
2024-05-30 20:37:42 +0900

    wave読込部分の実装(todo:sampleRateのSetter)

--- src/Wave.ts
+++ src/Wave.ts
@@ -10,6 +10,8 @@ export default class Wave {
   private header: WaveHeader;
   /**wavデータ */
   private data_: Array<number> | null;
+  /**wavデータのRch、UTAUでは通常使用しない */
+  private rData_: Array<number> | null;
 
   /**
    *
@@ -17,15 +19,68 @@ export default class Wave {
    */
   constructor(data: ArrayBuffer) {
     this.header = new WaveHeader(data);
-    if (data.byteLength < this.header.dataIndex) {
+    if (data.byteLength <= this.header.dataIndex) {
       console.warn("このwavはヘッダ部分しかありません。");
       this.data_ = null;
-    }else if(data.slice(this.header.dataIndex).byteLength % this.blockSize !==0){
-        console.warn("このwavのデータ部分に欠損があります。");
-        this.data_ = null;
-    }else{
-        this.data_=new Array();
+      this.rData_ = null;
+      return;
+    } else if (
+      data.slice(this.header.dataIndex).byteLength % this.blockSize !==
+      0
+    ) {
+      console.warn(
+        "このwavのデータ部分に欠損があるため読み込まれませんでした。"
+      );
+      this.data_ = null;
+      this.rData_ = null;
+      return;
     }
+    const frames: number =
+      data.slice(this.header.dataIndex).byteLength / this.blockSize;
+    const dv = new DataView(data.slice(this.header.dataIndex));
+    this.data_ = new Array();
+    if (this.channels === 2) {
+      this.rData_ = new Array();
+    } else {
+      this.rData_ = null;
+    }
+    for (let i = 0; i < frames; i++) {
+      if (this.bitDepth === 8) {
+        this.data_.push(dv.getInt8(i * this.blockSize));
+        if (this.channels === 2) {
+          this.rData_?.push(dv.getInt8(i * this.blockSize + 1));
+        }
+      } else if (this.bitDepth === 16) {
+        this.data_.push(dv.getInt16(i * this.blockSize, true));
+        if (this.channels === 2) {
+          this.rData_?.push(dv.getInt16(i * this.blockSize + 2, true));
+        }
+      } else if (this.bitDepth === 24) {
+        this.data_.push(this.GetInt24(dv, i * this.blockSize));
+        if (this.channels === 2) {
+          this.rData_?.push(this.GetInt24(dv, i * this.blockSize + 3));
+        }
+      } else if (this.bitDepth === 32) {
+        this.data_.push(dv.getInt32(i * this.blockSize, true));
+        if (this.channels === 2) {
+          this.rData_?.push(dv.getInt32(i * this.blockSize + 4, true));
+        }
+      }
+    }
+  }
+
+  /**
+   * 24bitのwavデータを整数に変換する
+   * @param dv wavのデータ部
+   * @param index 読込開始バイト、このindexから3バイト分を読み込む
+   * @returns frameの値
+   */
+  GetInt24(dv: DataView, index: number): number {
+    let value = dv.getUint16(index, true) + dv.getUint8(index + 2) * 256 ** 2;
+    if (value >= 2 ** 23) {
+      value = value - 2 ** 24;
+    }
+    return value;
   }
 
   /**wavの総バイト数-8 */
@@ -36,6 +91,33 @@ export default class Wave {
   get channels(): number {
     return this.header.channels;
   }
+
+  /**
+   * チャンネル数
+   * this.channels===valueの場合何もしない。
+   * this.channels===2,value===1の場合、this.rDataをnullにする。
+   * this.channels===1,value===2の場合、this.dataをthis.rDataに複製する。
+   * チャンネル数を変更した場合、this.blockSizeやthis.bytePerSecも変更する。
+   * @param value 1か2
+   */
+  set channels(value: number) {
+    if (this.channels === value) {
+      return;
+    } else if (value === 1) {
+      this.header.channels = value;
+      this.rData_ = null;
+      this.header.blockSize = (value * this.bitDepth) / 8;
+      this.header.bytePerSec = this.blockSize * this.sampleRate;
+    } else if (value === 2) {
+      if (this.data_ !== null) {
+        this.header.channels = value;
+        this.rData_ = [...this.data_];
+        this.header.blockSize = (value * this.bitDepth) / 8;
+        this.header.bytePerSec = this.blockSize * this.sampleRate;
+      }
+    }
+  }
+
   /**サンプリング周波数 */
   get sampleRate(): number {
     return this.header.sampleRate;
@@ -51,5 +133,41 @@ export default class Wave {
   /**ビット深度 */
   get bitDepth(): number {
     return this.header.bitDepth;
+  }
+
+  /**
+   * ビット深度
+   * this.data、this.rData、this.blockSize,this.bytePerSecもあわせて変更する。
+   * data部分の変更は、data = data / (2 ** (this.bitDepth-1)) *  (2 ** (value-1))
+   * @param value 8か16か24か32。それ以外の値の場合何もしない。
+   */
+  set bitDepth(value: number) {
+    if (value % 8 !== 0 || value > 32) {
+      //waveのbit深度は8,16,24,32のためそれ以外の値は何もせず返す。
+      return;
+    }
+    const newData = new Array();
+    this.data_?.forEach((d) => {
+      newData.push((d / 2 ** this.bitDepth) * 2 ** value);
+    });
+    this.data_ = newData;
+    if (this.rData !== null) {
+      const newRData = new Array();
+      this.rData_?.forEach((d) => {
+        newRData.push((d / 2 ** (this.bitDepth - 1)) * 2 ** (value - 1));
+      });
+      this.rData_ = newRData;
+    }
+    this.header.bitDepth = value;
+    this.header.blockSize = (this.channels * this.bitDepth) / 8;
+    this.header.bytePerSec = this.blockSize * this.sampleRate;
+  }
+  /**データ部分 */
+  get data(): Array<number> | null {
+    return this.data_;
+  }
+  /**wavデータのRch、UTAUでは通常使用しない */
+  get rData(): Array<number> | null {
+    return this.rData_;
   }
 }
