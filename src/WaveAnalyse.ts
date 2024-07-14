0854228b1809ac5391f7fe5628afe34c3cd14cfe
delta_kimigatame <sankaku.kuro@gmail.com>
2024-07-14 21:12:04 +0900

    暫定実装(非常に遅い)

--- src/WaveAnalyse.ts
+++ src/WaveAnalyse.ts
@@ -3,7 +3,7 @@
  */
 
 import Complex from "./Complex";
-import { fft } from "./lib/fft";
+import { fft, ifft } from "./lib/fft";
 
 export default class WaveAnalyse {
   constructor() {}
@@ -122,4 +122,81 @@ export default class WaveAnalyse {
     }
     return window;
   }
+  /**
+   * 基本周波数を求める。
+   * @param data 1で正規化されたwavのデータ
+   * @param sampleRate wavのサンプリング周波数。default,44100
+   * @param fftSize fftのフレーム数、2のべき乗である必要がある。default,9048
+   * @param stepSize f0を求めるフレームの範囲
+   * @param f0_floor 最低周波数
+   * @param f0_ceil 最大周波数
+   * @param threshold 推定値が有意かどうか判定する閾値
+   * @returns `threshold`以上の有意な推定値が得られれば周波数、それ以外の場合0のArray
+   */
+  F0(
+    data: Array<number>,
+    sampleRate: number = 44100,
+    fftSize: number = 9048,
+    stepSize: number = 256,
+    f0_floor: number = 40,
+    f0_ceil: number = 800,
+    threshold: number = 0.2
+  ): Array<number> {
+    const f0: Array<number> = new Array();
+    /** fftに渡すためにwavのデータを複素数にしたもの */
+    const complexValue: Array<Complex> = new Array();
+    /** fftした際の最低周波数に相当するインデックス */
+    const T0_floor = Math.ceil(sampleRate / f0_floor);
+    /** fftした際の最高周波数に相当するインデックス */
+    const T0_ceil = Math.floor(sampleRate / f0_ceil);
+    data.forEach((v) => {
+      complexValue.push(new Complex(v, 0));
+    });
+    const halfFftSize: number = Math.floor(fftSize / 2);
+    for (let i = 0; i < halfFftSize; i++) {
+      complexValue.unshift(new Complex(0, 0));
+      complexValue.push(new Complex(0, 0));
+    }
+    for (
+      let i = halfFftSize;
+      // i < complexValue.length - halfFftSize;
+      i < 1+halfFftSize;
+      i += stepSize
+    ) {
+      /** 当該indexの範囲のデータ */
+      const targetFrames = complexValue.slice(i - halfFftSize, i + halfFftSize);
+      /** fftしたデータ */
+      const spec = fft(targetFrames);
+      /** fft結果のパワー */
+      const power: Array<Complex> = new Array();
+      spec.forEach((s) => {
+        power.push(new Complex(s.re ** 2 + s.sub ** 2, 0));
+      });
+      /** iFFTした結果 */
+      const autocorrelation_ = ifft(power);
+      /** 最高周波数～最低周波数の間のiFFT結果の実部を1で正規化したもの */
+      const autocorrelation: Array<number> = new Array();
+      autocorrelation_.slice(0, fftSize / 2).forEach((a, i) => {
+        if (i < T0_ceil || i > T0_floor) {
+          autocorrelation.push(0);
+        } else {
+          autocorrelation.push(a.re / autocorrelation_[0].re);
+        }
+      });
+      /** ピーク値 */
+      const max = autocorrelation.reduce(aryMax);
+      /** ピーク値のindex */
+      const maxIndex = autocorrelation.indexOf(max);
+      if (max >= threshold) {
+        f0.push(sampleRate/maxIndex);
+      } else {
+        f0.push(0);
+      }
+    }
+    return f0;
+  }
 }
+
+const aryMax = (a: number, b: number): number => {
+  return Math.max(a, b);
+};
